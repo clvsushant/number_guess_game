dd799fa433d0b03abfb6247f34e528421dea3334
sushant <densushant@gmail.com>
2022-01-05 16:03:22 +0530

    done

---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var app = express();
+
+app.use(
+  session({
+    secret: 'guessinggameexpress',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, './static')));
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+app.get('/play', function (req, res) {
+  if (typeof req.session.number == 'undefined') {
+    req.session.number = Math.floor(Math.random() * 100);
+  }
+  if (typeof req.session.count == 'undefined') {
+    req.session.count = 0;
+  }
+  res.render('verify', { session: req.session });
+});
+
+app.post('/verify', function (req, res) {
+  if (req.session.number > req.body.guess) {
+    req.session.reply = 'low';
+    req.session.count++;
+  } else if (req.session.number < req.body.guess) {
+    req.session.reply = 'high';
+    req.session.count++;
+  } else if (req.session.number == req.body.guess) {
+    req.session.status = 'gameover';
+    req.session.reply = 'correct';
+    req.session.count++;
+  } else {
+    req.session.reply = 'invalid';
+  }
+  res.redirect('/play');
+});
+
+app.get('/reset', function (req, res) {
+  req.session.destroy();
+  res.redirect('/play');
+});
+
+app.listen(4000, function () {
+  console.log('listening on port 4000');
+});
